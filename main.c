33489c895a9c7c775ce1148dd523aadadfdb83ea
Amal818 <amalcona8@gmail.com>
2023-05-24 16:35:35 -0700

    commited

--- main.c
+++ main.c
@@ -2,43 +2,44 @@
 
 /**
  * main - entry point
- * Return: 1 on error and 0 on success
- * @co: arg count
- * @ve: arg vector
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int co, char **ve)
+int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
-	int df = 2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 			"add $3, %0"
-			: "=r" (df)
-			: "r" (df));
+			: "=r" (fd)
+			: "r" (fd));
 
-	if (co == 2)
+	if (ac == 2)
 	{
-		df = open(ve[1], O_RDONLY);
-		if (df == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(ve[0]);
+				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(ve[1]);
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = df;
+		info->readfd = fd;
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, ve);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
 
